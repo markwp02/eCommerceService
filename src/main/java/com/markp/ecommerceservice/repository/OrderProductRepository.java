6d691e3e71728e7c7e2a3b3bc493ac93fad3f7de
Mark <markwp02@gmail.com>
2023-11-14 15:46:00 +1100

    Removed Order Product endpoint
    - OrderProduct updated by CustomerOrder REST http calls
    - Endpoints for OrderProduct are therefore, not required

--- src/main/java/com/markp/ecommerceservice/repository/OrderProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.markp.ecommerceservice.repository;
-
-import com.markp.ecommerceservice.entity.OrderProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Integer> {
-
-}
